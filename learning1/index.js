87ead33131c6ff34fcc63a51a710802d3d7c374d
Gondola <Dr.Wheats@gmail.com>
2022-04-22 20:06:14 -0400

    v1

--- learning1/index.js
+++ learning1/index.js
@@ -1,35 +1,53 @@
 // in a new folder be sure to run "npm init -y" and "npm install puppeteer"
 const puppeteer = require("puppeteer")
-const fs = require("fs/promises")
+const fs = require("fs.promises")
+const nodemailer = require('nodemailer')
+const CronJob = require('cron').CronJob;
+const cheerio = require('cheerio')
 
 async function start() {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
-    await page.goto("https://learnwebcode.github.io/practice-requests/")
+    await page.goto("https://www.amazon.ca")
+    await page.type("#twotabsearchtextbox", "e scooter")
+    await Promise.all([page.click("#nav-search-submit-button"), page.waitForNavigation()])
+    // logs our search query to console
+    const myResult = await page.$eval(".a-color-state", element => element.textContent)
 
-    const names = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll(".info strong")).map(x => x.textContent)
-    })
-    await fs.writeFile("names.txt", names.join("\r\n"))
 
-    await page.click("#clickme")
-    const clickedData = await page.$eval("#data", el => el.textContent)
-    console.log(clickedData)
+    await console.log(`Results for : ${myResult}:`)
+    const box = await page.$$eval('s-result-item', guy => guy)
+    let i = 0;
+    box.forEach(guy => console.log(guy.textContent)
+    );
+    console.log(box)
 
-    const photos = await page.$$eval("img", imgs => {
-        return imgs.map(x => x.src)
-    })
+    // div.sg-col-4-of-12:nth-child(6)
 
-    await page.type("#ourfield", "blue")
-    await Promise.all([page.click("#ourform button"), page.waitForNavigation()])
-    const info = await page.$eval("#message", el => el.textContent)
 
-    console.log(info)
-
-    for (const photo of photos) {
-        const imagepage = await page.goto(photo)
-        await fs.writeFile(photo.split("/").pop(), await imagepage.buffer())
-    }
+    // const names = await page.evaluate(() => {
+    //    return Array.from(document.querySelectorAll(".info strong")).map(x => x.textContent)
+    // })
+    // await fs.writeFile("names.txt", names.join("\r\n"))
+    //
+    // await page.click("#clickme")
+    // const clickedData = await page.$eval("#data", el => el.textContent)
+    // console.log(clickedData)
+    //
+    //
+    //
+    // await page.type("#ourfield", "blue")
+    // await Promise.all([page.click("#ourform button"), page.waitForNavigation()])
+    // const info = await page.$eval("#message", el => el.textContent)
+    //
+    // console.log(info)
+    // const photos = await page.$$eval("img", imgs => {
+    //     return imgs.map(x => x.src)
+    // })
+    // for (const photo of photos) {
+    //     const imagepage = await page.goto(photo)
+    //     await fs.writeFile(photo.split("/").pop(), await imagepage.buffer())
+    // }
 
     await browser.close()
 }
